994d1cfb0d7f6345a8d26fec7b234623d5182d4d
Andrew Jackura <ajackura@google.com>
2016-03-18 12:59:53 -0400

    Update logger_syslog.go
    Push // +build linux darwin up to top of file

--- logger_syslog.go
+++ logger_syslog.go
@@ -1,3 +1,5 @@
+// +build linux darwin
+
 /*
 Copyright 2016 Google Inc. All Rights Reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,8 +12,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-// +build linux darwin
 
 package logger
 
