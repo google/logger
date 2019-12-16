78f2abff04b51c123f15ee7cc686981064cbd255
Garrett Massman <gmassman20@gmail.com>
2019-12-16 15:34:52 -0800

    Don't write to stderr if ioutil.Discard used

--- logger.go
+++ logger.go
@@ -19,6 +19,7 @@ package logger
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 	"sync"
@@ -93,8 +94,11 @@ func Init(name string, verbose, systemLog bool, logFile io.Writer) *Logger {
 	if el != nil {
 		eLogs = append(eLogs, el)
 	}
+
 	// Windows services don't have stdout/stderr. Writes will fail, so try them last.
-	eLogs = append(eLogs, os.Stderr)
+	if logFile != ioutil.Discard {
+		eLogs = append(eLogs, os.Stderr)
+	}
 	if verbose {
 		iLogs = append(iLogs, os.Stdout)
 		wLogs = append(wLogs, os.Stdout)
